6feef0892d187877db4b9dc950191a2ff0e4a5a5
Soimo <jnr@bk.ru>
2018-11-02 11:41:06 +0300

    Commit

--- Array/Program.cs
+++ Array/Program.cs
@@ -10,6 +10,64 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Пример работы с массивами \n");
+            Console.WriteLine("\nВведите длинну строки (натуральное число)");
+            int x = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("\nВведите длинну столбца (натуральное число)");
+            int y = int.Parse(Console.ReadLine());
+
+            int[,] arr1 = new int[x, y];
+
+            Console.WriteLine("\nЗаполните таблицу первого массива");
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write("Индекс[" + i + "," + j + "]: ");
+                    arr1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            
+
+            int[,] arr2 = new int[x, y];
+
+            Console.WriteLine("\nЗаполните таблицу второго массива");
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write("Индекс[" + i + "," + j + "]: ");
+                    arr2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int[,] arr3 = new int[x, y];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    arr3[i, j] = arr1[i, j] + arr2[i, j];
+                }
+
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.WriteLine("Значение массива с индексом [ " + i + "," + j + " ] равно: " + arr3[i,j]);
+
+                }
+            }
+            Console.ReadLine();
         }
+
+
+        
     }
 }
